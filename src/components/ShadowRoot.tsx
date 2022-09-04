981891c264e9c5a5592da2950eda262907bbc9dd
dhythm <yuta.okada.20@gmail.com>
2022-09-04 19:46:06 +0900

    wip

--- src/components/ShadowRoot.tsx
+++ src/components/ShadowRoot.tsx
@@ -11,28 +11,30 @@ export const ShadowRoot: FC<Props> = ({ children }) => {
   useEffect(() => {
     let unsubscribeCapture: any;
     let unsubscribeCustom: any;
-    if (!didInit && ref.current?.childNodes) {
-      didInit = true;
-      unsubscribeCapture = addEventListenerToAllNodes(
-        ref.current?.childNodes,
-        "click",
-        (e) => {
-          console.count(
-            `----- shadow root: capturing at ${
-              (e.currentTarget as any)?.nodeName
-            } -----`
-          );
-          e.stopImmediatePropagation();
-          e.target?.dispatchEvent(
-            new MouseEvent("custom-click", {
-              bubbles: false,
-              cancelable: false,
-              composed: false,
-            })
-          );
-        },
-        true
+    const listener = (e: Event) => {
+      console.count(
+        `----- shadow root: capturing at ${
+          (e.currentTarget as any)?.nodeName
+        } -----`
       );
+      e.stopImmediatePropagation();
+      e.target?.dispatchEvent(
+        new MouseEvent("custom-click", {
+          bubbles: false,
+          cancelable: false,
+          composed: false,
+        })
+      );
+    };
+    if (!didInit && ref.current?.childNodes) {
+      const didInitCurrent = didInit;
+      didInit = true;
+      ref.current.addEventListener("click", listener, true);
+      unsubscribeCapture = () => {
+        if (didInitCurrent) {
+          ref.current?.removeEventListener("click", listener, true);
+        }
+      };
       unsubscribeCustom = addEventListenerToAllNodes(
         ref.current?.childNodes,
         "custom-click",
@@ -53,9 +55,110 @@ export const ShadowRoot: FC<Props> = ({ children }) => {
     }
 
     return () => {
+      console.count("----- clean up -----");
       unsubscribeCapture?.();
       unsubscribeCustom?.();
     };
   }, [ref, didInit]);
   return <div ref={ref}>{children}</div>;
 };
+
+type DOMEventName =
+  | "abort"
+  | "afterblur" // Not a real event. This is used by event experiments.
+  // These are vendor-prefixed so you should use the exported constants instead:
+  // 'animationiteration' |
+  // 'animationend |
+  // 'animationstart' |
+  | "beforeblur" // Not a real event. This is used by event experiments.
+  | "beforeinput"
+  | "blur"
+  | "canplay"
+  | "canplaythrough"
+  | "cancel"
+  | "change"
+  | "click"
+  | "close"
+  | "compositionend"
+  | "compositionstart"
+  | "compositionupdate"
+  | "contextmenu"
+  | "copy"
+  | "cut"
+  | "dblclick"
+  | "auxclick"
+  | "drag"
+  | "dragend"
+  | "dragenter"
+  | "dragexit"
+  | "dragleave"
+  | "dragover"
+  | "dragstart"
+  | "drop"
+  | "durationchange"
+  | "emptied"
+  | "encrypted"
+  | "ended"
+  | "error"
+  | "focus"
+  | "focusin"
+  | "focusout"
+  | "fullscreenchange"
+  | "gotpointercapture"
+  | "hashchange"
+  | "input"
+  | "invalid"
+  | "keydown"
+  | "keypress"
+  | "keyup"
+  | "load"
+  | "loadstart"
+  | "loadeddata"
+  | "loadedmetadata"
+  | "lostpointercapture"
+  | "message"
+  | "mousedown"
+  | "mouseenter"
+  | "mouseleave"
+  | "mousemove"
+  | "mouseout"
+  | "mouseover"
+  | "mouseup"
+  | "paste"
+  | "pause"
+  | "play"
+  | "playing"
+  | "pointercancel"
+  | "pointerdown"
+  | "pointerenter"
+  | "pointerleave"
+  | "pointermove"
+  | "pointerout"
+  | "pointerover"
+  | "pointerup"
+  | "popstate"
+  | "progress"
+  | "ratechange"
+  | "reset"
+  | "resize"
+  | "scroll"
+  | "seeked"
+  | "seeking"
+  | "select"
+  | "selectstart"
+  | "selectionchange"
+  | "stalled"
+  | "submit"
+  | "suspend"
+  | "textInput" // Intentionally camelCase. Non-standard.
+  | "timeupdate"
+  | "toggle"
+  | "touchcancel"
+  | "touchend"
+  | "touchmove"
+  | "touchstart"
+  // These are vendor-prefixed so you should use the exported constants instead:
+  // 'transitionend' |
+  | "volumechange"
+  | "waiting"
+  | "wheel";
