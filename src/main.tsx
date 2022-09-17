54f2b4227de47d8f9dd7865457ff61e2f0a86b9c
dhythm <yuta.okada.20@gmail.com>
2022-09-17 10:40:30 +0900

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -5,47 +5,95 @@ import "./index.css";
 
 export const listeners: any[] = [];
 
-[window, document, Element.prototype].forEach((target) => {
+// [window, document, Element.prototype].forEach((target) => {
+// [Element.prototype].forEach((target) => {
+[EventTarget.prototype].forEach((target) => {
   const nativeAddEventListener = target.addEventListener;
-  target.addEventListener = (
+  const nativeRemoveEventListener = target.removeEventListener;
+
+  target.addEventListener = function (
     ...args: [
       string,
       EventListenerOrEventListenerObject,
       boolean | AddEventListenerOptions | undefined
     ]
-  ) => {
-    // nativeAddEventListener(...args);
-    nativeAddEventListener(
-      args[0],
-      (e) => {
-        const paths = e.composedPath();
-        const isShadowDOM = paths.some((path: any) => path?.shadowRoot);
-        if (!isShadowDOM) {
-          (args[1] as any)(e);
-          return;
-        }
-        // e.stopImmediatePropagation();
-        if (e.type === "click") {
-          console.log({
-            target,
-            event: e,
-            type: args[0],
-            handler: args[1],
-            options: args[2],
-          });
-        }
+  ) {
+    this.addEventListener = nativeAddEventListener;
+    // this.addEventListener(...args);
+    console.log(args);
+    // this.addEventListener(
+    //   args[0],
+    //   (e) => {
+    //     const paths = e.composedPath();
+    //     const isShadowDOM = paths.some((path: any) => path?.shadowRoot);
+    //     if (!isShadowDOM) {
+    //       (args[1] as any)(e);
+    //       return;
+    //     }
+    //     if (e.type === "click") {
+    //       console.log({
+    //         target,
+    //         event: e,
+    //         type: args[0],
+    //         handler: args[1],
+    //         options: args[2],
+    //       });
+    //     }
+    //     e.stopImmediatePropagation();
 
-        listeners.push({
-          target,
-          event: e,
-          type: args[0],
-          handler: args[1],
-          options: args[2],
-        });
-      },
-      args[2]
-    );
+    //     listeners.push({
+    //       target,
+    //       event: e,
+    //       type: args[0],
+    //       handler: args[1],
+    //       options: args[2],
+    //     });
+    //   },
+    //   args[2]
+    // );
   };
+  // target.addEventListener = (
+  //   ...args: [
+  //     string,
+  //     EventListenerOrEventListenerObject,
+  //     boolean | AddEventListenerOptions | undefined
+  //   ]
+  // ) => {
+  //   nativeAddEventListener(...args);
+  //   // nativeAddEventListener(
+  //   //   args[0],
+  //   //   (e) => {
+  //   //     const paths = e.composedPath();
+  //   //     const isShadowDOM = paths.some((path: any) => path?.shadowRoot);
+  //   //     if (!isShadowDOM) {
+  //   //       (args[1] as any)(e);
+  //   //       return;
+  //   //     }
+  //   //     e.stopImmediatePropagation();
+
+  //   //     if (e.type === "click") {
+  //   //       console.log({
+  //   //         target,
+  //   //         event: e,
+  //   //         type: args[0],
+  //   //         handler: args[1],
+  //   //         options: args[2],
+  //   //       });
+  //   //     }
+
+  //   //     listeners.push({
+  //   //       target,
+  //   //       event: e,
+  //   //       type: args[0],
+  //   //       handler: args[1],
+  //   //       options: args[2],
+  //   //     });
+  //   //   },
+  //   //   args[2]
+  //   // );
+  // };
+  console.log({ nativeAddEventListener });
+  console.log({ pollutedAddEventListener: target.addEventListener });
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
