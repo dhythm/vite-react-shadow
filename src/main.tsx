476843df01d11412416d185ae56e9e52819af637
dhythm <yuta.okada.20@gmail.com>
2022-09-19 22:13:55 +0900

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -40,6 +40,12 @@ export const listeners: any[] = [];
             options: args[2],
           });
           handler = function (event: any) {
+            // if (event.type?.endswith("click")) {
+            //   console.log({ event, args });
+            // }
+            if (event.type === "react-click") {
+              console.log({ event, args });
+            }
             (args[1] as any)(event);
           };
           listeners.push({
